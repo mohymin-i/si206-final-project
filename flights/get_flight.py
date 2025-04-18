97e433ad8a828573e170b962279b94034c9e74ec
neeyam-m <neeyam32@gmail.com>
2025-04-18 12:25:31 -0400

    flights format done

---
+++ flights/get_flight.py
@@ -0,0 +1,39 @@
+import requests
+import json
+import sqlite3
+import sys
+
+def main(depart, arrive, date):
+    url = 'https://test.api.amadeus.com/v2/shopping/flight-offers'
+    params = {
+    "originLocationCode": depart,
+    "destinationLocationCode": arrive,
+    "departureDate": date,
+    "adults": 1,
+    "nonStop": "true",
+    "max": 10
+    }
+    headers = {
+    'Authorization': 'Bearer BJTt7l3MBln2oGkedVT7peVqDIdn'
+    }
+
+    response = requests.get(url, params=params, headers=headers)
+    data = response.json()
+    print(data)
+
+    with open("output.json", "w") as f:
+        try:
+            json.dump(data, f, indent=2)
+        except Exception as e:
+            print("failed to write response to file:", e)
+
+
+if __name__ == "__main__":
+    try:
+        depart, arrive, date = sys.argv[1:4]
+    except ValueError:
+        print("""Usage: python get_flight.py <DEPART LOCATION CODE> <ARRIVAL LOCATION CODE> <DEPARTURE_DATE>
+- Ex: $python get_flight.py DTW SEA 2025-05-05
+- Dates must be in the future """)
+        sys.exit(1)
+    main(depart, arrive, date)
